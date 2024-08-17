4aa26cacfa9436f69fd7e86eddb0dff38c3f3e9e
DoDung <doquangdung1782004@gmail.com>
2024-08-17 16:44:21 +0700

    Create Category Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
 import "./App.css";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  return <Home />;
+  return <ProductList />;
 }
 
 export default App;
