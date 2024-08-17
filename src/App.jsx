e44587607bfff0adf4e534a4d0c5e2e7d380b9d9
DoDung <doquangdung1782004@gmail.com>
2024-08-17 17:11:59 +0700

    Create Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
 import "./App.css";
 import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 function App() {
-  return <ProductList />;
+  return <Product />;
 }
 
 export default App;
